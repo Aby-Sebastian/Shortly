f55fca5ef7b6ff858a13f31d45121c5f2257d69c
Aby Sebastian <abysebastian98@gmail.com>
2022-09-23 23:37:11 +0530

    code refactoring

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,4 @@
 from django.forms import ModelForm, TextInput, EmailInput, PasswordInput, CharField
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from taggit.forms import TagField
 from .models import Links
 
 
@@ -27,7 +24,6 @@ class CreateCustomLinkForm(ModelForm):
             # 'short_url' : TextInput(attrs={'class':"form-control", 'id':"floatingInputEmail", 'name':"short_url", 'placeholder':"Custom short url"}),
             "tags": TextInput(attrs={'class':"form-control url-inp", "placeholder" : "Add tags here. ","data-role": "tagsinput"})
                     }
-    # tags = TagField()
 
     def save(self, commit=True):
         m = super(CreateCustomLinkForm, self).save(commit=False)
@@ -36,20 +32,3 @@ class CreateCustomLinkForm(ModelForm):
             m.save()
         return m
 
-
-class CreateUserForm(UserCreationForm):
-    password1 = CharField(label="Password",widget=PasswordInput(attrs={'class':'form-control', 'id':'floatingPassword', 'placeholder':'Password', 'style':'margin-bottom: 0px; border-bottom-right-radius: 0; border-bottom-left-radius: 0;'}),
-                                    help_text="<small><ul><li>Your password can't be too similar to your other personal information.</li>\
-                                    <li>Your password must contain at least 8 characters.</li>\
-                                    <li>Your password can't be a commonly used password.</li>\
-                                    <li>Your password can't be entirely numeric.</li></ul></small>")
-    password2 = CharField(label="Password confirmation", widget=PasswordInput(attrs={'class': 'form-control', 'id':'floatingPassword2', 'placeholder':'Passoword'}),\
-                                help_text="<small>Enter the same password as before, for verification.</small>")
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password1', 'password2']
-        widgets = {
-            'username' : TextInput(attrs={'class':"form-control" ,'id':"floatingInput", 'name':"username", 'placeholder':"Username"}),
-            'email' : EmailInput(attrs={'class':"form-control", 'id':"floatingInputEmail", 'name':"email", 'placeholder':"name@example.com"}),
-                    }
--- app/views.py
+++ app/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render, HttpResponse, redirect, get_object_or_404,get_list_or_404
-from django.contrib.auth import authenticate, login, logout
+
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.http import JsonResponse
 from django.core.paginator import Paginator
-from django.core.mail import send_mail
-from django.conf import settings
+
 from django.utils import timezone
 import uuid
 import qrcode
 import qrcode.image.svg
 from io import BytesIO
-from .forms import CreateLinkForm, CreateUserForm, CreateCustomLinkForm
+from .forms import CreateLinkForm, CreateCustomLinkForm
 from files.forms import UploadFileForm
 from .models import Links, Customer
 from files.models import Files
@@ -344,51 +343,6 @@ def deleteFile(request):
 
 def temp(request):
 	return render(request, 'dashboard/temp.html')
-
-def registerUser(request):
-	
-	if request.method == 'POST':
-		form = CreateUserForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			username = form.cleaned_data.get('username')
-			
-			mail = request.POST.get('email')
-			subject = f'Welcome to Shortly'
-			message = f'Hi {username}, Welcome to Shortly. Thank you for registering in our website.'
-			email_from = settings.EMAIL_HOST_USER
-			recipient_list = [mail]
-			send_mail(subject, message, email_from, recipient_list)
-
-			messages.success(request, message=f'Welcome {username}, Your Account is created now')
-			return redirect('login')
-	else:
-		form = CreateUserForm()
-	context={'form':form}
-	return render(request,'main/register.html',context=context)
-
-
-
-def loginPage(request):
-	if request.method == 'POST':
-		username = request.POST.get('username')
-		password = request.POST.get('password')
-
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			return redirect('dashboard')
-		else:
-			messages.info(request, message='Username or Password is incorrect')
-			return redirect('login')
-	context = {}
-	return render(request, 'main/login.html',context=context)
-
-
-def logoutUser(request):
-	logout(request)
-	return redirect('index')
-
 
 def log(request):
 	return render(request, 'user/login.html')

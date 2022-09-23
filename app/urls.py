5e47ff3c7e9f6b148ce6c118e49eab21078952c9
Aby Sebastian <abysebastian98@gmail.com>
2022-09-23 23:56:15 +0530

    code refactoring

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -37,33 +36,6 @@ urlpatterns = [
 
     path('qr/<str:short_url>',views.qr_code,name='qr'),
     path('analytics/',views.analytics, name='analytics'),
-
-    # user login, logout and register
-    path('register/',views.registerUser,name='register'),
-    path('login/',views.loginPage,name='login'),
-    path('logout/',views.logoutUser,name='logout'),
-
-    # password change
-    path('change_password/', 
-        auth_views.PasswordChangeView.as_view(template_name='password/change_password.html'), 
-        name="change_password"),
-    path('password_change_done/', 
-        auth_views.PasswordChangeDoneView.as_view(template_name='password/password_change_done.html'), 
-        name="password_change_done"),
-
-    # password reset 
-    path('reset_password/', 
-        auth_views.PasswordResetView.as_view(template_name='password/password_reset.html'), 
-        name="reset_password"),
-    path('reset_password_sent/', 
-        auth_views.PasswordResetDoneView.as_view(template_name='password/password_reset_sent.html'), 
-        name='password_reset_done'),
-    path('reset/<uidb64>/<token>', 
-        auth_views.PasswordResetConfirmView.as_view(template_name='password/password_reset_form.html'), 
-        name='password_reset_confirm'),
-    path('reset_password_complete/', 
-        auth_views.PasswordResetCompleteView.as_view(template_name='password/password_reset_done.html'), 
-        name='password_reset_complete'),
 
     path('<str:pk>/',views.go,name='go'),
 ]
